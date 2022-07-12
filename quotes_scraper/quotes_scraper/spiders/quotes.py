554de6fb6e939db463f804855bb48ecade1cb616
bryhans <bryhansalazarpacsi@gmail.com>
2022-07-12 15:16:19 -0500

    Video 13 Platzi

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -4,6 +4,7 @@ import scrapy
 #Titulo = //h1/a/text()
 #Citas = //span[@class="text" and @itemprop="text"]/text()
 #Top ten tags = //div[contains(@class, "tags-box")]//span[@class"tag-item"]/a/text()
+#Next page button =  '//ul[@class="pager"]//li[@class="next"]/a/@href'
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -12,26 +13,17 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n\n')
-
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo: {title}')        
-        print('\n\n')
-
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Citas: ')
-        for quotes in quotes:
-            print(f'- {quote}')
-        print('\n\n')
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()       
+
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
 
 
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class"tag-item"]/a/text()').getall()       
-        print('Top ten tags: ')
-        for tag in top_ten_tags:
-            print(f'- {tag}')
-        print('\n\n')
-
-        print('\n\n\n')
-        print('*' * 10)
-
+        next_page_buttom_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_buttom_link:
+            yield response.follow(next_page_buttom_link)
