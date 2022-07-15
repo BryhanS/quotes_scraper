90c8857db332f8f68ffbab6687a7c31c71ff51fe
bryhans <bryhansalazarpacsi@gmail.com>
2022-07-15 00:38:40 -0400

    siquiente envio

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,9 +1,12 @@
+from pickle import TRUE
 import scrapy
 
 #Titulo = //h1/a/text()
 #Citas = //span[@class="text" and @itemprop="text"]/text()
 #Top ten tags = //div[contains(@class, "tags-box")]//span[@class"tag-item"]/a/text()
 #Next page button =  '//ul[@class="pager"]//li[@class="next"]/a/@href'
+#Author = //small[@class="author" and @itemprop="author"]/text()
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -13,7 +16,13 @@ class QuotesSpider(scrapy.Spider):
 
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUEST':24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['bryhan@fedora.pe'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'PepitoMartinez',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
 
@@ -24,22 +33,30 @@ class QuotesSpider(scrapy.Spider):
 
 
         next_page_buttom_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        author = response.xpath('//small[@class="author" and @itemprop="author"]/text()').getall()
+
         if next_page_buttom_link:
             yield response.follow(next_page_buttom_link, callback=self.parse_ony_quotes, cb_kwargs= {'quotes': quotes})
 
         else:
             yield{
-                'quotes': quotes
+                'quotes': f'- {quotes}',
+                'author': author
             }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()       
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()       
+
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
 
         yield {
             'title': title,
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
 
